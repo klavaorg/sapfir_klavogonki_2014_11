4ccaf5c4e07946ec2aff31d8320e69b66295c1ea
Eugen Soloviov <suenot@gmail.com>
2014-11-08 00:26:46 +0300

    не получается

--- js/base.js
+++ js/base.js
@@ -10,26 +10,23 @@ $(document).ready(function() {
 	var green = ['Reset82', 'Felicia555', 'GoodLoki', 'FreeAvenger', 'SnowMen'];
 
 	// EXERSIZES
-	var ex1 = {};
-	ex1['key'] = "testing";
-	// var ex2, ex3, ex4, ex5, ex6, ex7, ex8, ex9, ex10, ex11, ex12, ex13, ex14, ex15, ex16, ex17, ex18, ex19, ex20, ex21, ex22, ex23, ex24, ex25, ex26, ex27, ex28, ex29, ex30, ex31, ex32, ex33, ex34, ex35, ex36, ex37, ex38, ex40, ex41, ex42, ex43, ex44, ex45, ex46, ex47, ex48, ex49, ex50, ex51, ex52, ex53, ex54;
+	// var data = [];
+	var data = {};
 
 	// PARSING
-	// $('.right .btn').click(function(e){
-	// 	e.preventDefault();
-	// 	$('#players .player').each(function(){
-	// 		// $(this).find('.nick_content').text();
-	// 		// ex1.push({
-	// 		// 	key: "keyName",
-	// 		// 	value: "the value"
-	// 		// });
-	// 		// ex1.push({
-	// 		// 	key: "lolo",
-	// 		// 	value: "the value"
-	// 		// });
-	// 		ex1['key'] = "testing";
-	// 		ex1['key2'] = "testing";
-	// 	});
-	// 	console.log(ex1);
-	// });
+	$('.right .btn').click(function(e){
+		e.preventDefault();
+		$('#players .player').each(function(){
+			var $this = $(this);
+			$('#players .player').each(function() {
+				// data.push({
+				// 	name: $(this).find('.nick_content').text(), // ник
+				// 	speed: $(this).find('.bitmore').text() // скорость
+				// });
+				data[$(this).find('.nick_content').text()] = $(this).find('.bitmore').text();
+			});
+
+		});
+		console.log(data);
+	});
 });
