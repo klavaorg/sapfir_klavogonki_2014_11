2a7724dd2afcd0459fa9463ce0640e0ba65b5dd4
Eugen Soloviov <suenot@gmail.com>
2014-11-07 13:03:21 +0300

    start'

--- js/base.js
+++ js/base.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
+	// fix iframe
 	$('.left .btn').click(function(e){
 		e.preventDefault();
 		$('iframe').attr( 'src', $(this).attr('href') );
+	});
+
+	// TEAMS
+	var red = ['AvtandiLine', 'ibnteo', 'PROFI', 'Alkhor', 'Софья_Андреевна'];
+	var green = ['Reset82', 'Felicia555', 'GoodLoki', 'FreeAvenger', 'SnowMen'];
+
+	// EXERSIZES
+	var ex1, ex2, ex3, ex4, ex5, ex6, ex7, ex8, ex9, ex10, ex11, ex12, ex13, ex14, ex15, ex16, ex17, ex18, ex19, ex20, ex21, ex22, ex23, ex24, ex25, ex26, ex27, ex28, ex29, ex30, ex31, ex32, ex33, ex34, ex35, ex36, ex37, ex38, ex40, ex41, ex42, ex43, ex44, ex45, ex46, ex47, ex48, ex49, ex50, ex51, ex52, ex53, ex54;
+
+	// PARSING
+	$('.right .btn').click(function(e){
+		e.preventDefault();
 	});
 });
