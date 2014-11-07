d7ca74f65819890e2692240f08fb88681bcb7cf2
Eugen Soloviov <suenot@gmail.com>
2014-11-07 11:47:45 +0300

    first markup

---
+++ js/base.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	// $('.btn-default').click(function(){
+	// 	$('iframe').onload = function () {
+	// 		$('iframe').contentWindow.animate({
+	// 			scrollTop: '0px'
+	// 		}, 'fast');
+	// 	}
+	// });
+});
